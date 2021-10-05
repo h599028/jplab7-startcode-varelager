a176571df16ba244f1bfb76c1b8211c79a825454
h599028 <599028@stud.hvl.no>
2021-10-05 13:37:15 +0200

    Oppgave B1 fullført

--- startcode/src/no/hvl/dat100/varelager/Vare.java
+++ startcode/src/no/hvl/dat100/varelager/Vare.java
@@ -2,57 +2,73 @@ package no.hvl.dat100.varelager;
 
 public class Vare {
 
-	// TODO - objektvariable
+	private int varenr;
+	private String navn;
+	private double pris;
 	
 	public Vare(int varenr, String navn, double pris) {
-		
-		throw new TODO("Vare"); // REMOVE ME WHEN DONE
+		this.varenr = varenr;
+		this.navn = navn;
+		this.pris = pris;
 	}
 	
 	public int getVarenr() {
 		
-		throw new TODO("getVarenr");
+		return varenr;
 		
 	}
 	
 	public void setVarenr(int varenr) {
 		
-		throw new TODO("setVarenr");
+		this.varenr = varenr;
+		
 	}
 	
 	public String getNavn() {
 		
-		throw new TODO("getNavn");
+		return navn;
+		
 	}
 	
 	public void setNavn(String navn) {
 		
-		throw new TODO("setNavn");
+		this.navn = navn;
+		
 	}
 	
 	public double getPris() {
 		
-		throw new TODO("getPris");
+		return pris;
+
 	}
 	
 	public void setPris(double pris) {
 		
-		throw new TODO("setPris");
+		this.pris = pris;
+		
 	}
 	
 	public double beregnMoms() {
 		
-		throw new TODO("beregnMoms");
+		return 0.2 * pris;
+		
 	}
 	
 	public String toString() {
+		String s = "Vare [varenr=" + varenr + ", navn=" + navn + ", pris=" + pris + "]";
 		
-		throw new TODO("toString");
+		return s;
+		
 	}
 	
 	public boolean erBilligereEnn(Vare v) {
 		
-		throw new TODO("erBilligereEnn");
+		if (this.pris < v.getPris()) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 	
 }
