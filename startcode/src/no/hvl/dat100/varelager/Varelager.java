2c4b229a44eebd7c6dff7e9120646999f6264cf2
h599028 <599028@stud.hvl.no>
2021-10-06 11:14:49 +0200

    Oppgave B2a fullført

--- startcode/src/no/hvl/dat100/varelager/Varelager.java
+++ startcode/src/no/hvl/dat100/varelager/Varelager.java
@@ -4,43 +4,67 @@ public class Varelager {
 
 	protected Vare[] varer;
 	protected int antall;
-	
+
 	public Varelager(int n) {
-		
-		throw new TODO("Varelager");
+
+		varer = new Vare[n];
+
+		antall = 0;
 
 	}
-	
+
 	public Vare[] getVarer() {
-		
-		throw new TODO("getVarer");
+
+		return varer;
 
 	}
-	
+
 	public boolean leggTilVare(Vare v) {
-		
-		throw new TODO("leggTilVare");
 
+		if (antall < varer.length) {
+			varer[antall++] = v;
+			return true;
+		} else { 
+			return false;
+		}
+		
 	}
-	
+
 	public boolean leggTil(int varenr, String navn, double pris) {
-		
-		throw new TODO("leggTil");
+
+		if (antall < varer.length) {
+			varer[antall++] = new Vare(varenr, navn, pris);
+			return true;
+		} else {
+			return false;
+		}
 
 	}
-	
+
 	public Vare finnVare(int varenr) {
-			
-		throw new TODO("finnVare");
+
+		for (int i = 0; i < antall; i++) {
+			if (varer[i].getVarenr() == varenr) {
+				return varer[i];
+			}
+		}
+		
+		return null;
 
 	}
-	
+
 	private String SEP = "==============================";
-	
+
 	public void printVarelager() {
-				
-		throw new TODO("printVarelager");
+
+		System.out.println(SEP);
+		
+		for (int i = 0; i < antall; i++) {
+			System.out.println(varer[i].toString());
+		}
+		
+		System.out.println(SEP);
 
 	}
-	
+
 }
