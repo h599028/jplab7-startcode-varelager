b5b981c1bf2d9b212c6951f8e42084f29865774f
h599028 <599028@stud.hvl.no>
2021-10-06 11:31:55 +0200

    Oppgave B2b fullført

--- startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
+++ startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
@@ -4,25 +4,53 @@ public class VarelagerUtils {
 
 	public static Vare finnBilligste(Vare[] varer) {
 		
-		throw new TODO("finnBilligste");
+		Vare billigst = varer[0];
+		
+		for (Vare vare : varer) {
+			
+			if (vare.getPris() <= billigst.getPris()) {
+				billigst = vare;
+			}			
+			
+		}
+		
+		return billigst;
 
 	}
 	
 	public static double totalPris(Vare[] varer) {		
 		
-		throw new TODO("totalPris");
-
+		double total = 0;
+		
+		for (Vare vare : varer) {
+			total += vare.getPris();
+		}
+		
+		return total;
+		
 	}
 	
 	public static int[] finnVarenr(Vare[] varer) {
 				
-		throw new TODO("finnVarenr");
+		int[] varenr = new int[varer.length];
+		
+		for (int i = 0; i < varenr.length; i++) {
+			varenr[i] = varer[i].getVarenr();
+		}
+		
+		return varenr;
 
 	}
 	
 	public static double[] finnPrisDifferanser(Vare[] varer) {
-				
-		throw new TODO("finnPrisDifferanser");
+
+		double[] differanser = new double[varer.length - 1];
+		
+		for (int i = 0; i < differanser.length; i++) {
+			differanser[i] = Math.abs(varer[i].getPris() - varer[i + 1].getPris());
+		}
+		
+		return differanser;
 
 	}
 }
